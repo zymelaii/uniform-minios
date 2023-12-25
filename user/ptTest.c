22d9a48e69a4fff89c527ad308186bc7a89ca1a1
周上行Ryer <melaiiryer@gmail.com>
2023-12-25 14:17:40 +0800

    style: add .clang-format conf

--- user/ptTest.c
+++ user/ptTest.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 
-int global=0;
+int global = 0;
 
-char *str2,*str3;
+char *str2, *str3;
 
-
-void pthread_test1()
-{
-	int i;
-	//pthread(pthread_test2);
-	while(1)
-	{
-		printf("pth1");
-		printf("%d",++global);
-		printf(" ");
-		i=10000000;
-		while(--i){}
-	}
+void pthread_test1() {
+    int i;
+    // pthread(pthread_test2);
+    while (1) {
+        printf("pth1");
+        printf("%d", ++global);
+        printf(" ");
+        i = 10000000;
+        while (--i) {}
+    }
 }
 
 /*======================================================================*
@@ -24,18 +21,16 @@ void pthread_test1()
 added by xw, 18/4/27
  *======================================================================*/
 
-int main(int arg,char *argv[])
-{
-	int i=0;
+int main(int arg, char *argv[]) {
+    int i = 0;
 
-	pthread(pthread_test1);
-	while(1)
-	{
-		printf("init");
-		printf("%d",++global);
-		printf(" ");
-		i=10000000;
-		while(--i){}
-	}
-	return 0;
+    pthread(pthread_test1);
+    while (1) {
+        printf("init");
+        printf("%d", ++global);
+        printf(" ");
+        i = 10000000;
+        while (--i) {}
+    }
+    return 0;
 }
--- user/shell_0.c
+++ user/shell_0.c
@@ -7,25 +7,21 @@
 #include <proto.h>
 #include <stdio.h>
 
-int main(int arg, char *argv[])
-{
-	int stdin = open("dev_tty0", O_RDWR);
-	int stdout = open("dev_tty0", O_RDWR);
-	int stderr = open("dev_tty0", O_RDWR);
+int main(int arg, char *argv[]) {
+    int stdin  = open("dev_tty0", O_RDWR);
+    int stdout = open("dev_tty0", O_RDWR);
+    int stderr = open("dev_tty0", O_RDWR);
 
-	char buf[1024];
-	int pid;
-	int times = 0;
-	while (1)
-	{
-		printf("\nminiOS:/ $ ");
-		if (gets(buf) && strlen(buf) != 0)
-		{
-			if (exec(buf) != 0)
-			{
-				printf("exec failed: file not found!\n");
-				continue;
-			}
-		}
-	}
+    char buf[1024];
+    int  pid;
+    int  times = 0;
+    while (1) {
+        printf("\nminiOS:/ $ ");
+        if (gets(buf) && strlen(buf) != 0) {
+            if (exec(buf) != 0) {
+                printf("exec failed: file not found!\n");
+                continue;
+            }
+        }
+    }
 }
