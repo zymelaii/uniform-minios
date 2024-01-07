56af926d6228c526c94de8ae1f9640e8563aee8d
周上行Ryer <melaiiryer@gmail.com>
2024-01-07 12:18:28 +0800

    chore: fix ld order
    ATTENTION: current memory model is not fully guaranteed, kernel elf
    may crash into loader space, this is very dangerous since kernel
    stack originated in .bss section may overlap with the address where
    the loader put gdt. this commit force linker to locate kernel stack
    first to avoid overlap, but it is not a perfect solution and still
    has potential risk

--- project/rules-kernel.mk
+++ project/rules-kernel.mk
@@ -1,12 +1,15 @@
+# TODO: better solution
+KERNEL_START_FILE := $(OBJDIR)/kernel/kernel.asm.obj
+
 # build kernel
-$(KERNEL_FILE): $(KERNEL_OBJECTS) $(LIBRT_FILE) $(LIBGCC_FILE)
+$(KERNEL_FILE): $(KERNEL_START_FILE) $(KERNEL_OBJECTS) $(LIBRT_FILE) $(LIBGCC_FILE)
 	@echo -ne "[PROC] ld $(notdir $@)\r"
 	@mkdir -p $(@D)
 	@$(LD) $(LDFLAGS) -Ttext $(KERNEL_START_ADDR) -o $@ $^ -s
 	@echo -e "\e[1K\r\e[32m[DONE]\e[0m ld $(notdir $@)"
 
 # build kernel debug
-$(KERNEL_DEBUG_FILE): $(KERNEL_OBJECTS) $(LIBRT_FILE) $(LIBGCC_FILE)
+$(KERNEL_DEBUG_FILE): $(KERNEL_START_FILE) $(KERNEL_OBJECTS) $(LIBRT_FILE) $(LIBGCC_FILE)
 	@echo -ne "[PROC] ld $(notdir $@)\r"
 	@mkdir -p $(@D)
 	@$(LD) $(LDFLAGS) -Ttext $(KERNEL_START_ADDR) -o $@ $^
